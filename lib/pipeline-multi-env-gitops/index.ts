9dc851f88de33639ca0f752d7ce6fa6df96c8405
artur <estanqua@amazon.com>
2024-07-17 19:39:23 +0000

    gitowner

--- lib/pipeline-multi-env-gitops/index.ts
+++ lib/pipeline-multi-env-gitops/index.ts
@@ -101,6 +101,7 @@ export default class PipelineMultiEnvGitops {
 
         try {
             // TODO - add dynamic gitowner suport when using codeStar config const { gitOwner, gitRepositoryName } = await getRepositoryData();
+            const gitOwner = ‘estanqueiroa’
             const gitRepositoryName = 'cdk-eks-blueprints-patterns';
 
             blueprints.CodePipelineStack.builder()
